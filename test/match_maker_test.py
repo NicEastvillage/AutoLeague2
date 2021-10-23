5c85bed1e1603624d7b5ea09a49ba32cc8677ae5
lucas-emery <lucas.emery@hotmail.com>
2021-10-23 14:21:40 +0200

    New match making algorithm that considers expected bot performance (#6)
    * New match making algorithm that considers expected bot performance
    
    * Sort teams by MMR so it looks better in the overlay

--- test/match_maker_test.py
+++ test/match_maker_test.py
@@ -1,10 +1,12 @@
 import sys
-import unittest
 import numpy
+import unittest
+import itertools
+import trueskill
+import matplotlib.pyplot as plt
 from pathlib import Path
 from typing import Tuple, List
 
-import trueskill
 
 sys.path.insert(0, str(Path(__file__).parent.parent / 'autoleague'))
 
@@ -22,34 +24,59 @@ def get_trueskill_quality(players: Tuple[List[BotID], List[BotID]], rank_sys: Ra
     return trueskill.quality([blue_ratings, orange_ratings])
 
 
+def get_max_mmr_diff(players: Tuple[List[BotID], List[BotID]], rank_sys: RankingSystem) -> float:
+    mus = [rank_sys.get(bot).mu for bot in players[0] + players[1]]
+    return max(mus) - min(mus)
+
+
 NUM_ITERATIONS = 100
 
 
 class TestMatchMaker(unittest.TestCase):
 
-    def test_original_decide(self):
+    def _test_decide(self, decide_name, decide_function):
         rank_sys = RankingSystem.read(RESOURCES_FOLDER / '20210925212802_rankings.json')
         ticket_sys = TicketSystem.read(RESOURCES_FOLDER / '20210925212802_tickets.json', LeagueSettings())
-        bot_ids = rank_sys.ratings.keys()
-        quality_sum = 0
+        bot_ids = sorted(rank_sys.ratings.keys(), key=lambda id: rank_sys.ratings[id].mu)
+        qualities = []
+        max_mmr_diffs = []
+        matches_played = {bot_id: 0 for bot_id in bot_ids}
         for i in range(NUM_ITERATIONS):
-            players = MatchMaker.decide_on_players(bot_ids, rank_sys, ticket_sys)
+            players = decide_function(bot_ids, rank_sys, ticket_sys)
             quality = get_trueskill_quality(players, rank_sys)
-            quality_sum += quality
-        average = quality_sum / NUM_ITERATIONS
-        print(f'Original average quality: {average}')  # 0.395
+            qualities.append(quality)
+            max_mmr_diff = get_max_mmr_diff(players, rank_sys)
+            max_mmr_diffs.append(max_mmr_diff)
+            for bot_id in itertools.chain.from_iterable(players):
+                matches_played[bot_id] += 1
+
+        average = sum(qualities) / NUM_ITERATIONS
+        print(f'{decide_name} average quality: {average}')
+        plt.hist(qualities, bins=20, range=(0, 1))
+        plt.ylabel('matches')
+        plt.xlabel('quality')
+        plt.title(f'{decide_name} - Matches played by quality')
+        plt.show()
+        plt.hist(max_mmr_diffs, bins=10, range=(0, 100))
+        plt.ylabel('matches')
+        plt.xlabel('max MMR difference')
+        plt.title(f'{decide_name} - Matches played by MMR difference')
+        plt.show()
+        matches = matches_played.values()
+        plt.bar(range(len(matches)), matches)
+        plt.ylabel('matches')
+        plt.xlabel('bot (sorted by MMR)')
+        plt.title(f'{decide_name} - Matches played by bot')
+        plt.show()
+
+    def test_original_decide(self):
+        self._test_decide('Original', MatchMaker.decide_on_players)
 
     def test_new_decide(self):
-        rank_sys = RankingSystem.read(RESOURCES_FOLDER / '20210925212802_rankings.json')
-        ticket_sys = TicketSystem.read(RESOURCES_FOLDER / '20210925212802_tickets.json', LeagueSettings())
-        bot_ids = rank_sys.ratings.keys()
-        quality_sum = 0
-        for i in range(NUM_ITERATIONS):
-            players = MatchMaker.decide_on_players_2(bot_ids, rank_sys, ticket_sys)
-            quality = get_trueskill_quality(players, rank_sys)
-            quality_sum += quality
-        average = quality_sum / NUM_ITERATIONS
-        print(f'New average quality: {average}')  # 0.449
+        self._test_decide('Tarehart', MatchMaker.decide_on_players_2)
+
+    def test_rangler_decide(self):
+        self._test_decide('Rangler', MatchMaker.decide_on_players_3)
 
     def test_gamecount_equity(self):
         rank_sys = RankingSystem.read(RESOURCES_FOLDER / '20210925212802_rankings.json')
