f19306792bdbcc0cb7fcaacce27e9fce855beb11
NicEastvillage <nicoesterby@gmail.com>
2022-07-04 22:51:23 +0200

    Add surrogate class and match schedule js

--- autoleague/resources/overlay/match_history.js
+++ autoleague/resources/overlay/match_history.js
@@ -1,26 +1,28 @@
 const matchesTable = $("#matches-table");
 
 function updateMatchHistory(data) {
-
-    // Variables used to format table
-    let odd = true;
-
     matchesTable.html(data.matches
         .map(function (match) {
-            let odd_class = odd ? "odd" : "even";
-            odd = !odd;
 
-            let blue_crown_class = match.blue_goals < match.orange_goals ? "hide" : ""
-            let orange_crown_class = match.blue_goals > match.orange_goals ? "hide" : ""
+            let winClass = "";
+            if (match.winner >= 0) {
+                winClass = (match.winner == 0 ? "blue" : "orange");
+                winClass = winClass + (Math.abs(match.blue_score - match.orange_score) == 1 ? "-close" : "-win")
+            }
+
+            let blueSurrogateClasses = match.blue.map(bot => bot in match.surrogate ? "surrogate" : "")
+            let orangeSurrogateClasses = match.orange.map(bot => bot in match.surrogate ? "surrogate" : "")
 
             return `
-<div class="match-item ${odd_class}">
-    <div class="match-number"><p class="right">${match.index + 1})</p></div>
-    <div class="match-names blue"><p class="center">${match.blue_names.join(", ")}</p></div>
-    <div class="crown blue"><img class="${blue_crown_class}" src="images/match win blue.png" /></div>
-    <div class="match-mid"><p class="center">${match.blue_goals} VS ${match.orange_goals}</p></div>
-    <div class="crown orange"><img class="${orange_crown_class}" src="images/match win orange.png"/></div>
-    <div class="match-names orange"><p class="center">${match.orange_names.join(", ")}</p></div>
+<div class="match-item ${winClass}">
+    <p class="match-bot-name bot1 ${blueSurrogateClasses[0]}">${match.blue[0]}</p>
+    <p class="match-bot-name bot2 ${blueSurrogateClasses[1]}">${match.blue[1]}</p>
+    <p class="match-bot-name bot3 ${blueSurrogateClasses[2]}">${match.blue[2]}</p>
+    <p class="match-bot-name bot4 match-orange-name ${orangeSurrogateClasses[0]}">${match.orange[0]}</p>
+    <p class="match-bot-name bot5 match-orange-name ${orangeSurrogateClasses[1]}">${match.orange[1]}</p>
+    <p class="match-bot-name bot6 match-orange-name ${orangeSurrogateClasses[2]}">${match.orange[2]}</p>
+    <p class="match-score blue-score">${match.blue_score}</p>
+    <p class="match-score orange-score">${match.orange_score}</p>
 </div>`
                 }));
 }
