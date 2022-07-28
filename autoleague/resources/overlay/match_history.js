ffaa34e9dd7a089c8e04977af783df1e0a6ed8c2
NicEastvillage <nicoesterby@gmail.com>
2022-07-28 18:10:27 +0200

    Make matches single-line

--- autoleague/resources/overlay/match_history.js
+++ autoleague/resources/overlay/match_history.js
@@ -15,11 +15,11 @@ function updateMatchHistory(data) {
 
             return `
 <div class="match-item ${winClass}">
-    <p class="match-bot-name bot1 ${blueSurrogateClasses[0]}">${match.blue_names[0]}</p>
-    <p class="match-bot-name bot2 ${blueSurrogateClasses[1]}">${match.blue_names[1]}</p>
-    <p class="match-bot-name bot3 ${blueSurrogateClasses[2]}">${match.blue_names[2]}</p>
-    <p class="match-bot-name bot4 match-orange-name ${orangeSurrogateClasses[0]}">${match.orange_names[0]}</p>
-    <p class="match-bot-name bot5 match-orange-name ${orangeSurrogateClasses[1]}">${match.orange_names[1]}</p>
+    <p class="match-bot-name bot1 match-blue-name ${blueSurrogateClasses[0]}">${match.blue_names[0]},</p>
+    <p class="match-bot-name bot2 match-blue-name ${blueSurrogateClasses[1]}">${match.blue_names[1]},</p>
+    <p class="match-bot-name bot3 match-blue-name ${blueSurrogateClasses[2]}">${match.blue_names[2]}</p>
+    <p class="match-bot-name bot4 match-orange-name ${orangeSurrogateClasses[0]}">${match.orange_names[0]},</p>
+    <p class="match-bot-name bot5 match-orange-name ${orangeSurrogateClasses[1]}">${match.orange_names[1]},</p>
     <p class="match-bot-name bot6 match-orange-name ${orangeSurrogateClasses[2]}">${match.orange_names[2]}</p>
     <p class="match-score blue-score">${match.blue_goals}</p>
     <p class="match-score orange-score">${match.orange_goals}</p>
